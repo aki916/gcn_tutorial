c2c31744dd8e6f5b8470e6264f346d70ba1d9037
aki916 <aki916.genyo@gmail.com>
2021-04-05 19:22:39 +0900

    add link prediction

---
+++ link_prediction.py
@@ -0,0 +1,149 @@
+import itertools
+
+import numpy as np
+import scipy.sparse as sp
+
+import dgl
+import dgl.data
+import dgl.function as fn
+from dgl.nn import SAGEConv
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from sklearn.metrics import roc_auc_score
+
+class DotPredictor(nn.Module):
+    def forward(self, g, h):
+        with g.local_scope():
+            g.ndata['h'] = h
+            # Compute a new edge feature named 'score' by a dot-product between the
+            # source node feature 'h' and destination node feature 'h'.
+            g.apply_edges(fn.u_dot_v('h', 'h', 'score'))
+            # u_dot_v returns a 1-element vector for each edge so you need to squeeze it.
+            return g.edata['score'][:, 0]
+
+
+# ----------- 2. create model -------------- #
+# build a two-layer GraphSAGE model
+class GraphSAGE(nn.Module):
+    def __init__(self, in_feats, h_feats):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(in_feats, h_feats, 'mean')
+        self.conv2 = SAGEConv(h_feats, h_feats, 'mean')
+    
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = F.relu(h)
+        h = self.conv2(g, h)
+        return h
+
+def build_karate_club_graph():
+    # All 78 edges are stored in two numpy arrays. One for source endpoints
+    # while the other for destination endpoints.
+    src = np.array([1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 10, 10,
+        10, 11, 12, 12, 13, 13, 13, 13, 16, 16, 17, 17, 19, 19, 21, 21,
+        25, 25, 27, 27, 27, 28, 29, 29, 30, 30, 31, 31, 31, 31, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 33, 33, 33, 33, 33, 33,
+        33, 33, 33, 33, 33, 33, 33, 33, 33, 33])
+    dst = np.array([0, 0, 1, 0, 1, 2, 0, 0, 0, 4, 5, 0, 1, 2, 3, 0, 2, 2, 0, 4,
+        5, 0, 0, 3, 0, 1, 2, 3, 5, 6, 0, 1, 0, 1, 0, 1, 23, 24, 2, 23,
+        24, 2, 23, 26, 1, 8, 0, 24, 25, 28, 2, 8, 14, 15, 18, 20, 22, 23,
+        29, 30, 31, 8, 9, 13, 14, 15, 18, 19, 20, 22, 23, 26, 27, 28, 29, 30,
+        31, 32])
+    # Edges are directional in DGL; Make them bi-directional.
+    u = np.concatenate([src, dst])
+    v = np.concatenate([dst, src])
+    # Construct a DGLGraph
+    return dgl.DGLGraph((u, v))
+
+
+def get_graph():
+    # dataset = dgl.data.CoraGraphDataset()
+    # g = dataset[0]
+    g = build_karate_club_graph()
+    g.ndata['feat'] = torch.Tensor(np.zeros([len(g.nodes()), 10]))
+    return g
+
+def split_data(g):
+    # Split edge set for training and testing
+    u, v = g.edges()
+
+    eids = np.arange(g.number_of_edges())
+    eids = np.random.permutation(eids)
+    test_size = int(len(eids) * 0.1)
+    train_size = g.number_of_edges() - test_size
+    test_pos_u, test_pos_v = u[eids[:test_size]], v[eids[:test_size]]
+    train_pos_u, train_pos_v = u[eids[test_size:]], v[eids[test_size:]]
+
+    # Find all negative edges and split them for training and testing
+    adj = sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy())))
+    adj_neg = 1 - adj.todense() - np.eye(g.number_of_nodes())
+    neg_u, neg_v = np.where(adj_neg != 0)
+
+    neg_eids = np.random.choice(len(neg_u), g.number_of_edges() // 2)
+    test_neg_u, test_neg_v = neg_u[neg_eids[:test_size]], neg_v[neg_eids[:test_size]]
+    train_neg_u, train_neg_v = neg_u[neg_eids[test_size:]], neg_v[neg_eids[test_size:]]
+
+    train_g = dgl.remove_edges(g, eids[:test_size])
+
+    train_pos_g = dgl.graph((train_pos_u, train_pos_v), num_nodes=g.number_of_nodes())
+    train_neg_g = dgl.graph((train_neg_u, train_neg_v), num_nodes=g.number_of_nodes())
+
+    test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=g.number_of_nodes())
+    test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=g.number_of_nodes())
+
+    return train_g, train_pos_g, train_neg_g, test_pos_g, test_neg_g
+
+def compute_loss(pos_score, neg_score):
+    scores = torch.cat([pos_score, neg_score])
+    labels = torch.cat([torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])])
+    return F.binary_cross_entropy_with_logits(scores, labels)
+
+def compute_auc(pos_score, neg_score):
+    scores = torch.cat([pos_score, neg_score]).numpy()
+    labels = torch.cat(
+        [torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]).numpy()
+    return roc_auc_score(labels, scores)
+
+
+def main():
+
+    g = get_graph()
+    train_g, train_pos_g, train_neg_g, test_pos_g, test_neg_g = split_data(g)
+
+    model = GraphSAGE(train_g.ndata['feat'].shape[1], 16)
+    pred = DotPredictor()
+
+    # ----------- 3. set up loss and optimizer -------------- #
+    # in this case, loss will in training loop
+    optimizer = torch.optim.Adam(itertools.chain(model.parameters(), pred.parameters()), lr=0.01)
+
+    # ----------- 4. training -------------------------------- #
+    all_logits = []
+    for e in range(100):
+        # forward
+        h = model(train_g, train_g.ndata['feat'])
+        pos_score = pred(train_pos_g, h)
+        neg_score = pred(train_neg_g, h)
+        loss = compute_loss(pos_score, neg_score)
+        
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if e % 5 == 0:
+            print('In epoch {}, loss: {}'.format(e, loss))
+
+    # ----------- 5. check results ------------------------ #
+    with torch.no_grad():
+        pos_score = pred(test_pos_g, h)
+        neg_score = pred(test_neg_g, h)
+        print('AUC', compute_auc(pos_score, neg_score))
+
+
+
+if __name__ == '__main__':
+    main()
